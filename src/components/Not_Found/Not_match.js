63c8e00a13eebc2498ac837ab7a0e144eec6a4c1
ZIAN786 <zakirzian41@gmail.com>
2021-09-16 01:54:34 +0600

    add some work

---
+++ src/components/Not_Found/Not_match.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Not_match = () => {
+    return (
+        <div>
+            <h1>Page not found</h1>
+            <h3>404 Error!!!</h3>
+        </div>
+    );
+};
+
+export default Not_match;
